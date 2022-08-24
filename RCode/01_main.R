ae4d18bc4c814a008f37d44beecedcf78f38f55b
Andreas Höhn <hoehn@riseup.net>
2022-08-24 11:19:00 +0100

    updated date
    updated date

--- RCode/01_main.R
+++ RCode/01_main.R
@@ -2,7 +2,7 @@
 
 # Author: Andreas Hoehn
 # Version: 1.0
-# Date:  2022-08-22
+# Date:  2022-08-24
 # About: The Main Control File
 
 # ---------------------------------------------------------------------------- #
